5edf4a85ee9eda370e5cc82346adf3625e4a89ae
Andrew <andreimidari@gmail.com>
2025-07-25 14:56:16 +0300

    initial commit

---
+++ src/main/java/com/example/inventorymanagementsystem/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.inventorymanagementsystem.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Disable CSRF for testing (optional)
+                .headers(headers -> headers.frameOptions().disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().permitAll() // Require authentication for all requests
+                )
+                .httpBasic(); // Enable HTTP Basic authentication
+        return http.build();
+    }
+
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("user")
+//                .roles("ROLE_USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("admin")
+//                .roles("ROLE_ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
