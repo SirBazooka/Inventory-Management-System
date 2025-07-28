7c034ecccf787eb55ad5d3cf62e0b0e354e4b899
Andrew <andreimidari@gmail.com>
2025-07-28 12:33:20 +0300

    Added Admin and Staff roles into the SecurityConfig file.

--- src/main/java/com/example/inventorymanagementsystem/security/SecurityConfig.java
+++ src/main/java/com/example/inventorymanagementsystem/security/SecurityConfig.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,11 +17,24 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Disable CSRF for testing (optional)
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for testing (optional)
                 .headers(headers -> headers.frameOptions().disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/product/**").hasRole("STAFF")
+                        .requestMatchers(HttpMethod.POST, "/product/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/product/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/product/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/supplier/**").hasRole("STAFF")
+                        .requestMatchers(HttpMethod.POST, "/supplier/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/supplier/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/supplier/**").hasRole("ADMIN")
                         .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().permitAll() // Require authentication for all requests
                 )
@@ -31,17 +46,20 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
+
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
         // only updates the stock/quantity
         // cannot delete or manage
         UserDetails staff = User.withUsername("staff")
-                .password("staff")
-//                .roles("ROLE_USER")
+                .password(passwordEncoder.encode("staff"))
+                .roles("STAFF")
                 .build();
 
-        // full acces to the data
+        // full access to the data
         UserDetails admin = User.withUsername("admin")
-                .password("admin")
-//                .roles("ROLE_ADMIN")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN")
                 .build();
 
         // see the data
